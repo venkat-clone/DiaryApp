b72d18f8362082c4b659e036bc8778767a6cedc7
venkat-clone <lingampally.venkey@gmail.com>
2022-05-18 16:52:01 +0530

    mm

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -24,6 +24,7 @@ module.exports ={
         })
     },
     verifyAccessToken:(req,res,next)=>{
+        console.log(req.headers['authorization'])
         if(!req.headers['authorization']) return next(createError.Unauthorized('Unauthorized Request'))
         const authheader = req.headers['authorization']
         const bearearToken = authheader.split(' ')
